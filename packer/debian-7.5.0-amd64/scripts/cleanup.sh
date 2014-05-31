822e392329ae410964d14143f87d9189832b8474
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2014-05-31 15:52:22 +0200

    removes vmware builder, ruby and chef

--- packer/debian-7.5.0-amd64/scripts/cleanup.sh
+++ packer/debian-7.5.0-amd64/scripts/cleanup.sh
@@ -10,9 +10,6 @@ apt-get -y clean
 # Cleanup Virtualbox
 rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
 
-# Cleanup Chef
-rm -f /tmp/chef*deb
-
 # Remove unused locales
 rm -rf /usr/share/locale/{af,am,ar,as,ast,az,bal,be,bg,bn,bn_IN,br,bs,byn,ca,cr,cs,csb,cy,da,de,de_AT,dz,el,en_AU,en_CA,eo,es,et,et_EE,eu,fa,fi,fo,fr,fur,ga,gez,gl,gu,haw,he,hi,hr,hu,hy,id,is,it,ja,ka,kk,km,kn,ko,kok,ku,ky,lg,lt,lv,mg,mi,mk,ml,mn,mr,ms,mt,nb,ne,nl,nn,no,nso,oc,or,pa,pl,ps,pt,pt_BR,qu,ro,ru,rw,si,sk,sl,so,sq,sr,sr*latin,sv,sw,ta,te,th,ti,tig,tk,tl,tr,tt,ur,urd,ve,vi,wa,wal,wo,xh,zh,zh_HK,zh_CN,zh_TW,zu}
 
--- packer/ubuntu-14.04-server-amd64/scripts/vmtools.sh
+++ packer/ubuntu-14.04-server-amd64/scripts/vmtools.sh
@@ -13,18 +13,6 @@ virtualbox-iso|virtualbox-ovf)
     ln -s /opt/VBoxGuestAdditions-*/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
     ;;
 
-vmware-iso|vmware-ovf) 
-    mkdir /tmp/vmfusion
-    mkdir /tmp/vmfusion-archive
-    mount -o loop /home/vagrant/linux.iso /tmp/vmfusion
-    tar xzf /tmp/vmfusion/VMwareTools-*.tar.gz -C /tmp/vmfusion-archive
-    /tmp/vmfusion-archive/vmware-tools-distrib/vmware-install.pl --default
-    umount /tmp/vmfusion
-    rm -rf  /tmp/vmfusion
-    rm -rf  /tmp/vmfusion-archive
-    rm /home/vagrant/*.iso
-    ;;
-
 *)
     echo "Unknown Packer Builder Type >>$PACKER_BUILDER_TYPE<< selected."
     echo "Known are virtualbox-iso|virtualbox-ovf|vmware-iso|vmware-ovf."
